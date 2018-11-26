adc641444406ab0ef1c844e18e63e7eb65e7dd19
tinakarn <haiiplara@hotmail.com>
2018-11-26 18:12:57 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/products">Products</router-link> |
-      <router-link to="/users">Users</router-link>
-    </div>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+          <img src="https://bootstrap-vue.js.org/_nuxt/img/icon.f0a8c9e.png" width="30" height="30" alt="">
+          <router-link to="/" class="nav-item nav-link">Home</router-link> 
+          <router-link to="/about" class="nav-item nav-link">About</router-link> 
+          <router-link to="/products" class="nav-item nav-link">Products</router-link> 
+          <router-link to="/users" class="nav-item nav-link">Users</router-link>
+        </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
